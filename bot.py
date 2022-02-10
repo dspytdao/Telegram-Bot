db8d4624480e84f6f9d8034853c097971a06f202
Pavel Fedotov <66903336+Pfed-prog@users.noreply.github.com>
2022-02-10 22:55:25 +0300

    3commas added

--- bot.py
+++ bot.py
@@ -4,6 +4,20 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from app.utils import FirstTry
 
+from py3cw.request import Py3CW
+
+p3cw = Py3CW(
+    key=os.environ.get("KEY"), 
+    secret=os.environ.get("SECRET"),
+    request_options={
+        'request_timeout': 10,
+        'nr_of_retries': 1,
+        'retry_status_codes': [502],
+        'retry_backoff_factor': 0.1
+    }
+)
+
+
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -12,17 +26,18 @@ logger = logging.getLogger(__name__)
 
 PORT = int(os.environ.get('PORT', '8443'))
 
-datastorage = FirstTry()
+#datastorage = FirstTry()
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     # how do we pass class instance here
 
-    datastorage.add_counter()
+    #datastorage.add_counter()
 
     """Send a message when the command /start is issued."""
-    update.message.reply_text(f'Hi! number:{datastorage.counter}')
+    update.message.reply_text(f'Hi! Here is how the bot works.')
+    #update.message.reply_text(f'Hi! number:{datastorage.counter}')
 
 
 def help(update, context):
@@ -32,11 +47,20 @@ def help(update, context):
 
 
 def echo(update, context):
+    #https://core.telegram.org/bots/api#sendanimation
+    #https://stackoverflow.com/questions/35294948/telegram-python-chatbot-replying-with-an-animated-gif
     """Echo the user message."""
     context.bot.sendAnimation(chat_id=update.message.chat_id,
             animation="http://techslides.com/demos/sample-videos/small.mp4", ## that's just data from local gif file
             caption='That is your gif!',
             )
+
+    error, data = p3cw.request(
+    entity='marketplace', 
+    action='signals',
+    action_id = '184'
+    )
+    update.message.reply_text(f'{data}')
     update.message.reply_text(update.message.text)
 
 
